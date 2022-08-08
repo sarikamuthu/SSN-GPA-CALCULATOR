ec4484984fb09dd08d0b094ad75fd53b163a1126
Sarika22-E <sarikatech22@gmail.com>
2022-08-08 20:43:13 +0530

    first commit

---
+++ js/chem.js
@@ -0,0 +1,306 @@
+
+
+
+var courseName = "Chemical Engineering";
+
+var courseUnits = [{
+    year: '1',
+    sems: [{
+        sem: '1',
+        subs: [{
+            name: 'Communicative English ',
+            id: 'UEN1176',
+            c: 3
+        }, {
+            name: 'Algebra and Calculus',
+            id: 'UMA1176',
+            c: 4
+        }, {
+            name: 'Engineering Physics',
+            id: 'UPH1176',
+            c: 3
+        }, {
+            name: 'Engineering Chemistry',
+            id: 'UCY1176',
+            c: 3
+        }, {
+            name: 'Problem Solving and Programming in Python',
+            id: 'UGE1176',
+            c: 3
+        }, {
+            name: 'Engineering Graphics',
+            id: 'UGE1177',
+            c: 3
+        }, {
+            name: 'Programming in Python Lab ',
+            id: 'UGE1197',
+            c: 1.5
+        }, {
+            name: 'Physics and Chemistry Lab ',
+            id: 'UGS1177',
+            c: 1.5
+        }]
+    },
+    {
+        sem: '2',
+        subs: [{
+            name: 'Technical English',
+            id: 'UEN1276',
+            c: 3
+        }, {
+            name: 'Complex Functions and Laplace Transforms',
+            id: 'UMA1276',
+            c: 4
+        }, {
+            name: 'Physics of Materials ',
+            id: 'UPH1253',
+            c: 3
+        }, {
+            name: 'Chemistry for Technologists ',
+            id: 'UCY1251',
+            c: 3
+        }, {
+            name: 'Basic Mechanical Engineering',
+            id: 'UME1251',
+            c: 4
+        }, {
+            name: 'Principles of Chemical Engineering',
+            id: 'UCH1201',
+            c: 3
+        }, {
+            name: 'Design Thinking and Engineering Practices Lab',
+            id: 'UGE1297',
+            c: 1.5
+        }, {
+            name: 'Chemical Analysis Lab ',
+            id: 'UCH1211',
+            c: 1.5
+        }]
+    }
+    ]
+},
+{
+    year: '2',
+    sems: [{
+        sem: '3',
+        subs: [{
+            name: 'Transform Techniques & Partial Differential Equations  ',
+            id: 'UMA1376',
+            c: 4
+        }, {
+            name: 'Chemical Process Calculations  ',
+            id: 'UCH1301',
+            c: 3.5
+        }, {
+            name: 'Fluid Mechanics for Chemical Engineers ',
+            id: 'UCH1302',
+            c: 3.5
+        }, {
+            name: 'Physical & Organic Chemistry',
+            id: 'UCY1351',
+            c: 3
+        }, {
+            name: 'Principles of Electrical and Electronics Engineering ',
+            id: 'UEE1351',
+            c: 3
+        }, {
+            name: 'Solid Mechanics for Technologists ',
+            id: 'UCE1351',
+            c: 3
+        }, {
+            name: 'Electrical Engineering Lab',
+            id: 'UEE1371',
+            c: 2
+            
+        }, {
+            name: 'Mechanical Engineering Lab',
+            id: 'UME1371',
+            c: 2
+            
+        }]
+    },
+    {
+        sem: '4',
+        subs: [{
+            name: 'Statistics and Numerical Methods',
+            id: 'UMA1476',
+            c: 4
+        }, {
+            name: 'Chemical Engineering Thermodynamics I ',
+            id: 'UCH1401',
+            c: 3
+        }, {
+            name: 'Environmental Science ',
+            id: 'UCY1276',
+            c: 3
+        }, {
+            name: 'Mechanical Operations  ',
+            id: 'UCH1402',
+            c: 3
+        }, {
+            name: 'Chemical Process Industries ',
+            id: 'UCH1403',
+            c: 3
+        }, {
+            name: 'Heat Transfer ',
+            id: 'UCH1404',
+            c: 3
+        }, {
+            name: 'Fluid Mechanics Lab ',
+            id: 'UCH1411',
+            c: 2
+        }, {
+            name: 'Organic Chemistry Lab',
+            id: 'UCY1471',
+            c: 2
+        },]
+    }
+    ]
+},
+{
+    year: '3',
+    sems: [{
+        sem: '5',
+        subs: [{
+            id: "UCH1501",
+            c:3,
+            name: "Industrial Process Plant Safety  "
+        }, {
+            id: "UCH1502",
+            c:4,
+            name: "Chemical Engineering Thermodynamics II "
+        }, {
+            id: "UCH1503",
+            c:3.5,
+            name: "Mass Transfer I"
+        }, {
+            id: "UCH1504",
+            c:3.5,
+            name: "Chemical Reaction Engineering I"
+        }, {
+            id: "",
+            c:3,
+            name: "Open Elective - 1 "
+        }, {
+            id: "",
+            c:3,
+            name: "Professional Elective – 1 "
+        }, {
+            id: "UCH1511",
+            c:2,
+            name: "Mechanical Operations Lab",
+        }, {
+            id: "UCH1512",
+            c:2,
+            name: "Heat Transfer Lab ",
+        },
+        
+
+        
+        
+        ]
+    },
+    {
+        sem: '6',
+        subs: [{
+            
+                id: "UCH1601",
+                name: "Chemical Reaction Engineering II",
+                c:3.5
+            }, {
+                id: "UCH1602",
+                name: "Mass Transfer II ",
+                c:3.5
+            },
+            {
+                id: "UCH1603",
+                name: "Process Dynamics and Control",
+                c:3
+            },{
+                id: "UCH1604",
+                name:" Instrumental Methods of Analysis ", 
+                c:3
+            },
+            {
+                id: "",
+                name: "Open Elective - 2",
+                c:3
+            },{
+                id: "",
+                name: "Professional Elective – 2",
+                c:3
+            },  {
+                id: "UCH1611",
+                name: "Chemical Reaction Engineering Lab ",
+                c:2
+            }, {
+                id: "UCH1612",
+                name: "Mass Transfer Lab",
+                c:2
+            }, 
+        
+        ]
+    }
+    ]
+}, {
+    year: '4',
+    sems: [{
+        sem: '7',
+        subs: [{
+            name: 'Transport Phenomena ',
+            id: 'UCH1701',
+            c:3
+        }, {
+            name: 'Process Equipment Design ',
+            id: 'UCH1702',
+            c:3
+        }, {
+            name: 'Process Engineering Economics ',
+            id: 'UCH1703',
+            c:3
+        },  {
+            name: 'Chemical Process Modelling and Simulation  ',
+            id: 'UCH1704',
+            c:3
+        }, {
+            name: 'Professional Elective - 3',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Professional Elective - 4',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Process Control Lab ',
+            id: 'UCH1711',
+            c:2
+        },
+        {
+            name: 'Computational Programming Laboratory for Chemical Engineers',
+            id: 'UCH1712',
+            c:2
+        },
+        
+        ]
+    },
+    {
+        sem: '8',
+        subs: [{
+            name: 'Professional Elective - 5 ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective – 6',
+            id: '',
+            c:3
+        }, {
+            name: 'Project Work ',
+            id: 'UCH1818',
+            c:10
+        }, ]
+    }
+    ]
+}
+];
---
+++ js/ece.js
@@ -0,0 +1,314 @@
+
+
+
+var courseName = "Electronics and Communication Engineering";
+
+var courseUnits = [{
+    year: '1',
+    sems: [{
+        sem: '1',
+        subs: [{
+            name: 'Communicative English ',
+            id: 'UEN1176',
+            c: 3
+        }, {
+            name: 'Algebra and Calculus',
+            id: 'UMA1176',
+            c: 4
+        }, {
+            name: 'Engineering Physics',
+            id: 'UPH1176',
+            c: 3
+        }, {
+            name: 'Engineering Chemistry',
+            id: 'UCY1176',
+            c: 3
+        }, {
+            name: 'Problem Solving and Programming in Python',
+            id: 'UGE1176',
+            c: 3
+        }, {
+            name: 'Engineering Graphics',
+            id: 'UGE1177',
+            c: 3
+        }, {
+            name: 'Programming in Python Lab ',
+            id: 'UGE1197',
+            c: 1.5
+        }, {
+            name: 'Physics and Chemistry Lab ',
+            id: 'UGS1177',
+            c: 1.5
+        }]
+    },
+    {
+        sem: '2',
+        subs: [{
+            name: 'Technical English',
+            id: 'UEN1276',
+            c: 3
+        }, {
+            name: 'Complex Functions and Laplace Transforms',
+            id: 'UMA1276',
+            c: 4
+        }, {
+            name: 'Physics for Electronics Engineering',
+            id: 'UPH1277',
+            c: 3
+        }, {
+            name: 'Circuit Analysis ',
+            id: 'UEC1201',
+            c: 4
+        }, {
+            name: 'Basic Electrical, Electronics and Measurement Engineering',
+            id: 'UEE1253',
+            c: 3
+        }, {
+            name: 'Electronic Devices ',
+            id: 'UEC1202',
+            c: 3
+        }, {
+            name: 'Design Thinking and Engineering Practices Lab',
+            id: 'UGE1297',
+            c: 1.5
+        }, {
+            name: 'Circuits and Devices Lab ',
+            id: 'UEC1211',
+            c: 1.5
+        }]
+    }
+    ]
+},
+{
+    year: '2',
+    sems: [{
+        sem: '3',
+        subs: [{
+            name: 'Linear Algebra and Numerical Methods ',
+            id: 'UMA1351',
+            c: 4
+        }, {
+            name: 'Electronic Circuits I ',
+            id: 'UEC1301',
+            c: 3
+        }, {
+            name: 'Digital System Design',
+            id: 'UEC1302',
+            c: 3
+        }, {
+            name: 'Signals and Systems',
+            id: 'UEC1303',
+            c: 4
+        }, {
+            name: 'Object Oriented Programming and Data Structures ',
+            id: 'UCS1351',
+            c: 3
+        }, {
+            name: 'Electromagnetic Fields',
+            id: 'UEC1304',
+            c: 4
+        }, {
+            name: 'OOP and Data Structures Lab',
+            id: 'UCS1371',
+            c: 1.5
+            
+        }, {
+            name: 'Analog and Digital Circuits Lab ',
+            id: 'UEC1311',
+            c: 1.5
+            
+        }]
+    },
+    {
+        sem: '4',
+        subs: [{
+            name: 'Probability Theory and Stochastic processes',
+            id: 'UMA1477',
+            c: 4
+        }, {
+            name: 'Environmental Science',
+            id: 'UCY1276',
+            c: 3
+        }, {
+            name: 'Electronic Circuits II ',
+            id: 'UEC1401',
+            c: 3
+        }, {
+            name: 'Digital Signal Processing  ',
+            id: 'UEC1402',
+            c: 4
+        }, {
+            name: 'Linear Integrated Circuits',
+            id: 'UEC1403',
+            c: 3
+        }, {
+            name: 'Principles of Communication Systems ',
+            id: 'UCS1404',
+            c: 3
+        }, {
+            name: 'Electronic Circuits II and LIC Lab',
+            id: 'UEC1411',
+            c: 1.5
+        }, {
+            name: 'Digital Signal Processing Lab ',
+            id: 'UEC1412',
+            c: 1.5
+        },]
+    }
+    ]
+},
+{
+    year: '3',
+    sems: [{
+        sem: '5',
+        subs: [{
+            id: "UEC1501",
+            c:3,
+            name: "Digital Communication  "
+        }, {
+            id: "UEC1502",
+            c:3,
+            name: "Microprocessors, Microcontroller and Interfacing "
+        }, {
+            id: "UEC1503",
+            c:3,
+            name: "Transmission Lines and Waveguides"
+        }, {
+            id: "UCS1551",
+            c:3,
+            name: "Computer Architecture and Organization"
+        }, {
+            id: "UEE1551",
+            c:3,
+            name: "Control Systems Engineering "
+        }, {
+            id: "",
+            c:3,
+            name: "Professional Elective – 1 "
+        }, {
+            id: "UEC1511",
+            c:1,
+            name: "Analog and Digital Communication Lab",
+        }, {
+            id: "UEC1512",
+            c:1,
+            name: "Microprocessor and Microcontroller Lab",
+        },
+        {
+            id: "UEN1597",
+            c:1,
+            name: "Professional Communications Lab ",
+        },
+
+        
+        
+        ]
+    },
+    {
+        sem: '6',
+        subs: [{
+            
+                id: "UEC1601",
+                name: "Principles of VLSI Design",
+                c:3
+            }, {
+                id: "UEC1602",
+                name: "Wireless Communications ",
+                c:3
+            },
+            {
+                id: "UEC1603",
+                name: "System Design for IoT",
+                c:3
+            },{
+                id: "UEC1604",
+                name:" Communication Networks", 
+                c:4
+            },
+            {
+                id: "UEC1606",
+                name: "Machine Learning",
+                c:4
+            },{
+                id: "",
+                name: "Professional Elective – 2",
+                c:3
+            },  {
+                id: "UEC1611",
+                name: "VLSI Design Lab ",
+                c:1
+            }, {
+                id: "UEC1612",
+                name: "System Design for IoT Lab",
+                c:1
+            }, 
+        
+        ]
+    }
+    ]
+}, {
+    year: '4',
+    sems: [{
+        sem: '7',
+        subs: [{
+            name: 'High Frequency Communication Systems ',
+            id: 'UEC1701',
+            c:3
+        }, {
+            name: 'Microwave Engineering and Antennas',
+            id: 'UEC1702',
+            c:3
+        }, {
+            name: 'Professional Ethics and Management Practices',
+            id: 'UEC1703',
+            c:3
+        },  {
+            name: 'Professional Elective - 3 ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective - 4',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Open Elective - 1',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Wireless Communication Lab ',
+            id: 'UEC1711',
+            c:1
+        },
+        {
+            name: 'Microwave and Antennas Lab',
+            id: 'UEC1712',
+            c:1
+        },
+        {
+            name: 'Project Work Stage – I',
+            id: 'UEC1718',
+            c:1
+        }
+        ]
+    },
+    {
+        sem: '8',
+        subs: [{
+            name: 'Professional Elective - 5 ',
+            id: '',
+            c:3
+        }, {
+            name: 'Open Elective – 2',
+            id: '',
+            c:3
+        }, {
+            name: 'Project Work Stage - 2',
+            id: 'UEC1818',
+            c:9
+        }, ]
+    }
+    ]
+}
+];
