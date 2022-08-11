672f1c6731c80ab1b57fd4226079eff17c7fbe71
Sarika Esackimuthu <87271190+sarikamuthu@users.noreply.github.com>
2022-08-11 18:42:27 +0530

    Add files via upload

---
+++ js/mech.js
@@ -0,0 +1,306 @@
+
+
+
+var courseName = "Mechanical Engineering";
+
+var courseUnits = [{
+    year: '1',
+    sems: [{
+        sem: '1',
+        subs: [{
+            name: 'Communicative English ',
+            id: 'UEN1176',
+            c: 3
+        }, {
+            name: 'Algebra and Calculus',
+            id: 'UMA1176',
+            c: 4
+        }, {
+            name: 'Engineering Physics',
+            id: 'UPH1176',
+            c: 3
+        }, {
+            name: 'Engineering Chemistry',
+            id: 'UCY1176',
+            c: 3
+        }, {
+            name: 'Problem Solving and Programming in Python',
+            id: 'UGE1176',
+            c: 3
+        }, {
+            name: 'Engineering Graphics',
+            id: 'UGE1177',
+            c: 3
+        }, {
+            name: 'Programming in Python Lab ',
+            id: 'UGE1197',
+            c: 1.5
+        }, {
+            name: 'Physics and Chemistry Lab ',
+            id: 'UGS1177',
+            c: 1.5
+        }]
+    },
+    {
+        sem: '2',
+        subs: [{
+            name: 'Technical English',
+            id: 'UEN1276',
+            c: 3
+        }, {
+            name: 'Complex Functions and Laplace Transforms',
+            id: 'UMA1276',
+            c: 4
+        }, {
+            name: 'Material Science for Mechanical Engineering ',
+            id: 'UPH1252',
+            c: 3
+        }, {
+            name: 'Environmental Science  ',
+            id: 'UCY1276',
+            c: 3
+        }, {
+            name: 'Engineering Mechanics ',
+            id: 'UME1201',
+            c: 4
+        }, {
+            name: 'Fundamentals of Electrical and Electronics Engineering',
+            id: 'UEE1251',
+            c: 3
+        }, {
+            name: 'Design Thinking and Engineering Practices Lab',
+            id: 'UGE1297',
+            c: 2
+        }, {
+            name: 'Electrical and Electronics Engineering Lab  ',
+            id: 'UEE1271',
+            c: 2
+        }]
+    }
+    ]
+},
+{
+    year: '2',
+    sems: [{
+        sem: '3',
+        subs: [{
+            name: 'Transform Techniques & Partial Differential Equations  ',
+            id: 'UMA1376',
+            c: 4
+        }, {
+            name: 'Engineering Thermodynamics   ',
+            id: 'UME1301',
+            c: 4
+        }, {
+            name: 'Fluid Mechanics and Machinery ',
+            id: 'UME1302',
+            c: 3
+        }, {
+            name: 'Manufacturing Processes I ',
+            id: 'UME1303',
+            c: 4
+        }, {
+            name: 'Strength of Materials  ',
+            id: 'UME1304',
+            c: 3
+        }, {
+            name: 'Kinematics of Machinery ',
+            id: 'UME1305',
+            c: 3
+        }, {
+            name: 'Fluid Mechanics and Machinery Lab',
+            id: 'UME1311',
+            c: 1.5
+            
+        }, {
+            name: 'Strength of Materials Lab',
+            id: 'UME1312',
+            c: 1.5
+            
+        }]
+    },
+    {
+        sem: '4',
+        subs: [{
+            name: 'Statistics and Numerical Methods ',
+            id: 'UMA1476',
+            c: 4
+        }, {
+            name: 'Engineering Metallurgy',
+            id: 'UME1401',
+            c: 3
+        }, {
+            name: 'Manufacturing Processes II   ',
+            id: 'UME1402',
+            c: 4
+        }, {
+            name: 'Thermal Engineering   ',
+            id: 'UME1403',
+            c: 3
+        }, {
+            name: 'Dynamics of Machines  ',
+            id: 'UME1404',
+            c: 3
+        }, {
+            name: 'Professional Elective - 1',
+            id: '',
+            c: 3
+        }, {
+            name: 'Thermal Engineering Lab ',
+            id: 'UME1411',
+            c: 1.5
+        }, {
+            name: 'Dynamics Lab',
+            id: 'UME1412',
+            c: 1.5
+        },]
+    }
+    ]
+},
+{
+    year: '3',
+    sems: [{
+        sem: '5',
+        subs: [{
+            id: "UME1501",
+            c:4,
+            name: "Metrology and Measurements "
+        }, {
+            id: "UM1E502",
+            c:3,
+            name: "Computer Aided Design and Manufacturing "
+        }, {
+            id: "UME1503",
+            c:3,
+            name: "Design of Machine Elements "
+        }, {
+            id: "UME1504",
+            c:4,
+            name: "Heat and Mass Transfer "
+        }, {
+            id: "UGE1576",
+            c:3,
+            name: "Professional Ethics "
+        }, {
+            id: "",
+            c:3,
+            name: "Professional Elective – 2 "
+        }, {
+            id: "UME1511",
+            c:1.5,
+            name: "Heat Transfer Lab",
+        }, {
+            id: "UME1512",
+            c:1.5,
+            name: "Computer Aided Design and Manufacturing Lab ",
+        },
+        ]
+    },
+    {
+        sem: '6',
+        subs: [{
+            
+                id: "UME1601",
+                name: "Design of Mechanical Drives ",
+                c:3
+            }, {
+                id: "UME1602",
+                name: "Finite Element Analysis  ",
+                c:3
+            },
+            {
+                id: "UME1603",
+                name: "Automobile engineering ",
+                c:3
+            },{
+                id: "UME1604",
+                name:"Power Plant Engineering", 
+                c:3
+            },
+            {
+                id: "UME1605",
+                name: "Operations Research  ",
+                c:4
+            },{
+                id: "",
+                name: "Professional Elective – 3",
+                c:3
+            },  {
+                id: "UME1611",
+                name: "Simulation Lab   ",
+                c:1.5
+            }, {
+                id: "UME1612",
+                name: "Designand Fabrication Project ",
+                c:1.5            }, 
+            {
+                id: "UEN1597",
+                name: "Professional Communications Lab",
+                c:1
+            }, 
+        
+        ]
+    }
+    ]
+}, {
+    year: '4',
+    sems: [{
+        sem: '7',
+        subs: [{
+            name: 'Industrial Practices  ',
+            id: '',
+            c:3
+        }, {
+            name: 'Mechatronics and Automation  ',
+            id: 'UME1701',
+            c:3
+        }, {
+            name: 'Computer Integrated  Manufacturing  ',
+            id: 'UME1702',
+            c:3
+        },  {
+            name: 'Open Elective - 1 ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective - 4',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Professional Elective - 5',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Mechatronics and Automation Lab  ',
+            id: 'UCE1717',
+            c:1.5
+        },
+        {
+            name: 'Project Work – Phase I  ',
+            id: 'UCE1716',
+            c:1.5
+        },
+        
+        ]
+    },
+    {
+        sem: '8',
+        subs: [{
+            name: 'Open Elective - 2 ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective – 6',
+            id: '',
+            c:3
+        }, {
+            name: 'Project Work - Phase II',
+            id: 'UME1818',
+            c:10
+        }, ]
+    }
+    ]
+}
+];
