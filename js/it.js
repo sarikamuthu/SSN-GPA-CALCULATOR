646c372176879e0bf2f35df6228f7029cd0b6af8
Sarika Esackimuthu <87271190+sarikamuthu@users.noreply.github.com>
2023-02-06 21:33:03 +0530

    Add files via upload

---
+++ js/it.js
@@ -0,0 +1,304 @@
+
+
+
+var courseName = "Information Technology";
+
+var courseUnits = [{
+    year: '1',
+    sems: [{
+        sem: '1',
+        subs: [{
+            name: 'Communicative English ',
+            id: 'UEN1176',
+            c: 3
+        }, {
+            name: 'Algebra and Calculus',
+            id: 'UMA1176',
+            c: 4
+        }, {
+            name: 'Engineering Physics',
+            id: 'UPH1176',
+            c: 3
+        }, {
+            name: 'Engineering Chemistry',
+            id: 'UCY1176',
+            c: 3
+        }, {
+            name: 'Problem Solving and Programming in Python',
+            id: 'UGE1176',
+            c: 3
+        }, {
+            name: 'Engineering Graphics',
+            id: 'UGE1177',
+            c: 3
+        }, {
+            name: 'Programming in Python Lab ',
+            id: 'UGE1197',
+            c: 1.5
+        }, {
+            name: 'Physics and Chemistry Lab ',
+            id: 'UGS1177',
+            c: 1.5
+        }]
+    },
+    {
+        sem: '2',
+        subs: [{
+            name: 'Technical English',
+            id: 'UEN1276',
+            c: 3
+        }, {
+            name: 'Complex Functions and Laplace Transforms',
+            id: 'UMA1276',
+            c: 4
+        }, {
+            name: 'Physics for Information Science',
+            id: 'UPH1276',
+            c: 3
+        }, {
+            name: 'Environmental Science ',
+            id: 'UCY1276',
+            c: 3
+        }, {
+            name: 'Basic Electrical, Electronics and Measurement Engineering',
+            id: 'UEE1276',
+            c: 3.5
+        }, {
+            name: 'Fundamentals of C programming ',
+            id: 'UIT1201',
+            c: 3.5
+        }, {
+            name: 'Design Thinking and Engineering Practices Lab',
+            id: 'UGE1297',
+            c: 1.5
+        }, {
+            name: 'Programming in C Lab ',
+            id: 'UCS1211',
+            c: 1.5
+        }]
+    }
+    ]
+},
+{
+    year: '2',
+    sems: [{
+        sem: '3',
+        subs: [{
+            name: 'Discrete Mathematics ',
+            id: 'UMA1377',
+            c: 4
+        }, {
+            name: 'Digital Electronics',
+            id: 'UIT1301',
+            c: 4
+        }, {
+            name: 'Fundamentals of Data Structures',
+            id: 'UIT1302',
+            c: 3
+        }, {
+            name: 'Principles of analog and digital communication',
+            id: 'UIT1303',
+            c: 3
+        }, {
+            name: 'Database Management Systems and Applications',
+            id: 'UIT1304',
+            c: 3
+        }, {
+            name: 'Computer Organization',
+            id: 'UIT1351',
+            c: 3
+        }, {
+            name: 'Programming and Data Structures Lab - I',
+            id: 'UIT1311',
+            c: 2
+        }, {
+            name: 'Database Management Systems and Applications Lab ',
+            id: 'UIT1312',
+            c: 2
+            
+        }]
+            
+    
+    },
+    {
+        sem: '4',
+        subs: [{
+            name: 'Probability and Statistics',
+            id: 'UMA1478',
+            c: 4
+        }, {
+            name: 'Principles of Software Engineering',
+            id: 'UIT1401',
+            c: 3
+        }, {
+            name: 'Information Theory and Applications',
+            id: 'UIT1402',
+            c: 3
+        }, {
+            name: 'Microprocessors and Microcontrollers ',
+            id: 'UIT1403',
+            c: 3
+        }, {
+            name: 'Advanced Data Structures',
+            id: 'UIT1404',
+            c: 3
+        }, {
+            name: 'Algorithm Design and Analysis',
+            id: 'UIT1405',
+            c: 3
+        }, {
+            name: 'Microprocessor and Microcontroller Lab',
+            id: 'UIT1411',
+            c: 2
+        }, {
+            name: 'Programming and Data Structures Lab - II ',
+            id: 'UIT1412',
+            c: 2
+        }, ]
+    }]
+    
+},
+{
+    year: '3',
+    sems:[{
+        sem: '5',
+        subs: [{
+            id: "UIT1501",
+            c:3,
+            name: "Finite Automata Theory "
+        }, {
+            id: "UIT1502",
+            c:3,
+            name: "Principles of Operating Systems "
+        }, {
+            id: "UIT1503",
+            c:4,
+            name: "Computer Networks and Its Applications"
+        }, {
+            id: "UIT1504",
+            c:4,
+            name: "Introduction to Digital Signal Processing"
+        }, {
+            id: "UIT1505",
+            c:3,
+            name: "Artificial Intelligence Concepts and Algorithms"
+        }, {
+            id: "",
+            c:3,
+            name: "Professional Elective I  "
+        }, {
+            id: "UIT1511",
+            c:2,
+            name: "Software Design Lab",
+        }, {
+            id: "UIT1512",
+            c:2,
+            name: "Operating Systems Lab",
+        },
+
+        
+        
+        ]
+    },
+    {
+        sem: '6',
+        subs: [{
+            
+                id: "UIT1601",
+                name: "Principles of Complier Design",
+                c:4
+            }, {
+                id: "UIT1602",
+                name: "Web Programming",
+                c:3
+            },
+            {
+                id: "UIT1603",
+                name: "Big Data Engineering ",
+                c:3
+            },{
+                id: "UIT1604",
+                name:" Machine Learning Fundamentals  ", 
+                c:4
+            }, {
+                id: "",
+                name: "Open Elective I",
+                c:3
+            }, {
+                id: "",
+                name: "Professional Elective II ",
+                c:3
+            }, {
+                id: "UEN1497",
+                name: "Interpersonal Skills/Listening & Speaking ",
+                c:1
+            }, {
+                id: "UIT1611",
+                name: "Web Programming Lab",
+                c:2
+            }, 
+        
+        ]
+    }
+    ]
+},
+ {
+    year: '4',
+    sems: [{
+        sem: '7',
+        subs: [{
+            name: 'Management Principles and Practices',
+            id: 'UIT1701',
+            c:3
+        }, {
+            name: 'Cloud Computing and Virtualization ',
+            id: 'UIT1702',
+            c:3
+        }, {
+            name: 'Network Security',
+            id: 'UIT1703',
+            c:3
+        }, {
+            name: 'Open Elective II',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective III ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective IV ',
+            id: '',
+            c:3
+        },
+        {
+            name: 'Mobile Application Development Lab ',
+            id: 'UIT1711',
+            c:2
+        },
+        {
+            name: 'Professional Communication Lab',
+            id: 'UEN1597',
+            c:1
+        },
+        ]
+    },
+    {
+        sem: '8',
+        subs: [{
+            name: 'Professional Elective V ',
+            id: '',
+            c:3
+        }, {
+            name: 'Professional Elective VI',
+            id: '',
+            c:3
+        }, {
+            name: 'Project Work',
+            id: 'UIT1818',
+            c:10
+        }, ]
+    }
+    ]
+}
+];
